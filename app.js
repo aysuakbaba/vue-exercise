fac581b751ca8d793bac1af1b0462a466cd17988
aysuakbaba <aysuakbabaa@gmail.com>
2023-11-08 17:10:20 +0300

    rendering conditional content and lists

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false,
+      goals: [],
+      enteredGoalValue: "",
     };
   },
-  computed: {
-    // boxAClasses() {
-    //   return { active: this.boxASelected };
-    // },
-    // boxBClasses() {
-    //   return { active: this.boxBSelected };
-    // },
-    // boxCClasses() {
-    //   return { active: this.boxCSelected };
-    // },
-  },
   methods: {
-    boxSelected(box) {
-      if (box === "A") {
-        this.boxASelected = !this.boxASelected;
-      } else if (box === "B") {
-        this.boxBSelected = !this.boxBSelected;
-      } else {
-        this.boxCSelected = !this.boxCSelected;
-      }
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+      this.resetEnteredGoal();
+    },
+
+    resetEnteredGoal() {
+      this.enteredGoalValue = "";
+    },
+
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
     },
   },
 });
 
-app.mount("#styling");
+app.mount("#user-goals");
